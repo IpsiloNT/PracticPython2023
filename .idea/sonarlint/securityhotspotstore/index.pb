b3344ef6d319349ed0900785d52018be960416d1
Lobanov Kirill <ipsilont954@gmail.com>
2023-10-15 19:48:52 +0300

    stability

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,8 @@
+
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
+:
+
+users.json,b\9\b9a3cf24e48e943560b97c62600586fba3ad4f68
+7
+main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import json
 from prettytable import PrettyTable
 
-# Функция для сохранения данных в JSON-файл
-def save_data(data):
-    with open("users.json", "w") as f:
-        json.dump(data, f, indent=4)
-
 # Функция для загрузки данных из JSON-файла
 def load_data():
     try:
@@ -13,109 +8,91 @@ def load_data():
             data = json.load(f)
     except (FileNotFoundError, json.decoder.JSONDecodeError):
         data = []
+
+    # Добавьте поле "login_count" со значением 0, если его нет
+    for user in data:
+        user.setdefault("login_count", 0)
+
     return data
 
+# Функция для сохранения данных в JSON-файл
+def save_data(data):
+    with open("users.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+
+# Функция для проверки существования пользователя по логину
+def check_user_existence(login, data):
+    return any(user["login"] == login for user in data)
+
 # Функция для авторизации пользователя
-def login():
-    data = load_data()
-    login = input("Введите логин: ")
-    password = input("Введите пароль: ")
-
-    user = next((user for user in data if user["login"] == login and user["password"] == password), None)
-
-    if user:
-        if user["role"] == 1:
-            print("\033[95m\033[1mВы вошли как Администратор\033[0m")  # Сиреневый цвет и жирный текст
-            print("\033[94m\033[1m***Админ панель***\033[0m")  # Синий цвет и жирный текст
-            admin_menu()
-        else:
-            print("Вы вошли как Пользователь")
-    else:
-        print("Ошибка авторизации. Проверьте логин и пароль.")
-
-def admin_menu():
+def authenticate(data):
     while True:
-        print("\033[96m1.\033[0m Посмотреть данные пользователей")
-        print("\033[96m2.\033[0m Добавить нового пользователя")
-        print("\033[96m3.\033[0m Удалить пользователя")
-        print("\033[96m4.\033[0m Изменить данные пользователя")
-        print("\033[96m5.\033[0m Отключить пользователя")
-        print("\033[96m6.\033[0m Просмотр статистики")
-        print("\033[96m7.\033[0m Выход")
-        choice = input("Выберите действие: ")
+        user_login = input("Введите логин: ")
+        password = input("Введите пароль: ")
 
-        if choice == "1":
-            view_users()
-        elif choice == "2":
-            add_user()
-        elif choice == "3":
-            delete_user()
-        elif choice == "4":
-            change_user()
-        elif choice == "5":
-            disconnect_user()
-        elif choice == "6":
-            view_stat()
-        elif choice == "7":
+        user = next((user for user in data if user["login"] == user_login and user["password"] == password), None)
+
+        if user:
+            if user["status"] == "inactive":
+                print("Вы не можете войти, так как вы отключены от системы. Обратитесь к администратору.")
+                continue
+
+            print(f"Вы вошли как {'Администратор' if user['role'] == 1 else 'Пользователь'}")
+
+            # Увеличение счетчика входов для пользователя
+            user["login_count"] += 1
+
+            # Сохранение обновленных данных в JSON
+            save_data(data)
+
+            if user["role"] == 1:
+                admin_menu(data)
             break
         else:
-            print("Неверный выбор. Пожалуйста, выберите 1, 2, 3, 4, 5, 6 или 7.")
+            print("Ошибка авторизации. Проверьте логин и пароль.")
 
 # Функция для отображения данных всех пользователей с использованием PrettyTable
-def view_users():
-    data = load_data()
+def view_users(data):
     if not data:
         print("Нет зарегистрированных пользователей.")
     else:
         table = PrettyTable()
-        table.field_names = ["ID", "Фамилия", "Имя", "Логин", "Роль"]
+        table.field_names = ["ID", "Фамилия", "Имя", "Логин", "Пароль", "Роль", "Статус"]
 
         for user in data:
             role = "Администратор" if user["role"] == 1 else "Пользователь"
-            table.add_row([user["id"], user["surname"], user["name"], user["login"], role])
-
+            status = "Включен" if user["status"] == "active" else "Отключен"
+            table.add_row([user["id"], user["surname"], user["name"], user["login"], user["password"], role, status])
         print(table)
 
-def add_user():
-    data = load_data()
+# Функция для добавления нового пользователя
+def add_user(data):
+    last_name = input("Введите фамилию: ")
+    first_name = input("Введите имя: ")
 
     while True:
-        last_name = input("Введите фамилию: ")
-        first_name = input("Введите имя: ")
+        login = input("Введите логин: ")
+        if not login:
+            print("Логин не может быть пустым. Пожалуйста, введите логин.")
+        elif check_user_existence(login, data):
+            print("Пользователь с таким логином уже существует. Пожалуйста, выберите другой логин.")
+        else:
+            break
 
-        while True:
-            login = input("Введите логин: ")
-            if not login:
-                print("Логин не может быть пустым. Пожалуйста, введите логин.")
-            elif any(user["login"] == login for user in data):
-                print("Пользователь с таким логином уже существует. Пожалуйста, выберите другой логин.")
-            else:
-                break
+    while True:
+        password = input("Введите пароль: ")
+        if len(password) < 6:
+            print("Пароль должен содержать как минимум 6 символов. Пожалуйста, введите пароль заново.")
+        else:
+            break
 
-        while True:
-            password = input("Введите пароль: ")
-            if len(password) < 6:
-                print("Пароль должен содержать как минимум 6 символов. Пожалуйста, введите пароль заново.")
-            else:
-                break
-
-        while True:
-            role = input("Введите роль (0 для пользователя, 1 для администратора): ")
-            if role not in ("0", "1"):
-                print("Роль должна быть 0 или 1.")
-            else:
-                role = int(role)
-                break
-
-        print("Пожалуйста, проверьте введенные данные:")
-        print(f"Фамилия: {last_name}")
-        print(f"Имя: {first_name}")
-        print(f"Логин: {login}")
-        print(f"Пароль: {password}")
-        print(f"Роль: {'Администратор' if role == 1 else 'Пользователь'}")
-
-        confirmation = input("Все ли верно? (да/нет): ")
-        if confirmation.lower() == "да":
+    while True:
+        role = input("Введите роль (0 для пользователя, 1 для администратора): ")
+        if role not in ("0", "1"):
+            print("Роль должна быть 0 или 1.")
+        else:
+            role = int(role)
             break
 
     user_id = len(data) + 1
@@ -125,37 +102,56 @@ def add_user():
         "name": first_name,
         "login": login,
         "password": password,
-        "role": role
+        "role": role,
+        "status": "active"
     }
 
     data.append(user)
     save_data(data)
-    print("Пользователь успешно добавлен в базу данных!")
-    view_users()
+    print("Пользователь успешно добавлен в базу данных.")
+    view_users(data)
 
-def delete_user():
-    view_users()
-    data = load_data()
-    login_to_delete = input("Введите \033[4mлогин\033[0m пользователя, которого вы хотите удалить: ")
+# Функция для изменения статуса пользователя
+def change_user_status(data):
+    while True:
+        view_users(data)
+        login_to_change_status = input("Введите логин пользователя, статус которого вы хотите изменить (или нажмите Enter для отмены): ")
 
-    user_to_delete = None
-    for user in data:
-        if user["login"] == login_to_delete:
-            user_to_delete = user
+        if not login_to_change_status:
             break
+
+        user_to_change_status = next((user for user in data if user["login"] == login_to_change_status), None)
+
+        if user_to_change_status:
+            if user_to_change_status["status"] == "active":
+                user_to_change_status["status"] = "inactive"
+                print(f"Статус пользователя с логином '{login_to_change_status}' изменен на 'Отключен'.")
+            else:
+                user_to_change_status["status"] = "active"
+                print(f"Статус пользователя с логином '{login_to_change_status}' изменен на 'Включен'.")
+            save_data(data)
+        else:
+            print(f"Пользователь с логином '{login_to_change_status}' не найден.")
+
+# Функция для удаления пользователя
+def delete_user(data):
+    view_users(data)
+    login_to_delete = input("Введите логин пользователя, которого вы хотите удалить: ")
+
+    user_to_delete = next((user for user in data if user["login"] == login_to_delete), None)
 
     if user_to_delete:
         data.remove(user_to_delete)
         save_data(data)
         print(f"Пользователь с логином '{login_to_delete}' успешно удален.")
+        view_users(data)
     else:
         print(f"Пользователь с логином '{login_to_delete}' не найден.")
 
-
-def change_user():
-    view_users()
-    data = load_data()
-    login_to_change = input("Введите логин пользователя, данные которого вы хотите изменить: ")
+# Функция для изменения данных пользователя
+def change_user_data(data):
+    view_users(data)
+    login_to_change = input("Введите логин пользователя, данные которого вы хотите изменить (или нажмите Enter для отмены): ")
 
     user_to_change = next((user for user in data if user["login"] == login_to_change), None)
 
@@ -164,14 +160,20 @@ def change_user():
         new_last_name = input(f"Новая фамилия ({user_to_change['surname']}): ")
         new_first_name = input(f"Новое имя ({user_to_change['name']}): ")
         new_login = input(f"Новый логин ({user_to_change['login']}): ")
-        new_password = input(f"Новый пароль ({user_to_change['password']}): ")
+
+        while True:
+            new_password = input(f"Новый пароль ({user_to_change['password']}): ")
+            if new_password and len(new_password) < 6:
+                print("Пароль должен содержать как минимум 6 символов.")
+            else:
+                break
 
         if new_last_name:
             user_to_change['surname'] = new_last_name
         if new_first_name:
             user_to_change['name'] = new_first_name
         if new_login:
-            if any(user["login"] == new_login for user in data):
+            if check_user_existence(new_login, data):
                 print("Пользователь с таким логином уже существует.")
             else:
                 user_to_change['login'] = new_login
@@ -180,15 +182,36 @@ def change_user():
 
         save_data(data)
         print(f"Данные пользователя с логином '{login_to_change}' успешно изменены.")
-    else:
+        view_users(data)
+    elif login_to_change:
         print(f"Пользователь с логином '{login_to_change}' не найден.")
 
-def disconnect_user():
-    # Add code to disconnect a user from the database
-    return
 
-def view_stat():
-    return
+# Функция для администраторского меню
+def admin_menu(data):
+    while True:
+        print("\033[96m1.\033[0m Посмотреть данные пользователей")
+        print("\033[96m2.\033[0m Добавить нового пользователя")
+        print("\033[96m3.\033[0m Удалить пользователя")
+        print("\033[96m4.\033[0m Изменить данные пользователя")
+        print("\033[96m5.\033[0m Изменить статус пользователя (включен/отключен)")
+        print("\033[96m6.\033[0m Выход")
+        choice = input("Выберите действие: ")
+
+        if choice == "1":
+            view_users(data)
+        elif choice == "2":
+            add_user(data)
+        elif choice == "3":
+            delete_user(data)
+        elif choice == "4":
+            change_user_data(data)
+        elif choice == "5":
+            change_user_status(data)
+        elif choice == "6":
+            break
+        else:
+            print("Неверный выбор. Пожалуйста, выберите 1, 2, 3, 4, 5 или 6.")
 
 # Основной цикл программы
 while True:
@@ -197,7 +220,8 @@ while True:
     choice = input("Выберите действие: ")
 
     if choice == "1":
-        login()
+        data = load_data()
+        authenticate(data)
     elif choice == "2":
         break
     else:
